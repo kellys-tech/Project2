de275ccecc2ed56c6aea78a68fb64f0e39387e0d
Javier <javierperez@javiers-mbp.home>
2020-12-22 20:11:53 -0500

    eslint fix

--- models/Category.js
+++ models/Category.js
@@ -1,30 +1,30 @@
 const {Model,DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Category extends Model {};
+class Category extends Model {}
 
 Category.init(
-    {
-        id:{
-            type:DataTypes.INTEGER,
-            allowNull:false,
-            autoIncrement:true,
-            primaryKey:true
-        },
-        name:{
-type: DataTypes.STRING,
-allowNull:false
-        },
-        description:{
-            type:DataTypes.STRING,
-            allowNull:true
-        }
+  {
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      autoIncrement:true,
+      primaryKey:true
     },
-    {
-        sequelize,
+    name:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    description:{
+      type:DataTypes.STRING,
+      allowNull:true
+    }
+  },
+  {
+    sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-    }
-)
+  }
+);
