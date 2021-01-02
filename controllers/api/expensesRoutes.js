6ee854da7fb84c89367c0003d925d0a9ba3fe669
nbmc1991 <nbmc1991@gmail.com>
2021-01-02 10:39:55 -0500

    routing expenses

--- controllers/api/expensesRoutes.js
+++ controllers/api/expensesRoutes.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
-// const {Expense} = require('../../models');
+const { Expense, User, Category } = require('../../models');
 
-// router.create('/',()=>{
+router.get('/', async (req, res) => {
+    const blogInfo = await Expense.findAll({
+        include: [{ model: User }, { model: Category }],
+    });
+    return res.json(blogInfo);
+});
 
-// })
-
+router.delete('/:id', async (req, res) => {
+    try {
+        const expenseData = await Expense.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.params.id,
+            },
+        })
+        res.status(200).json(expenseData)
+    } catch (err) {
+        throw err;
+    }
+});
 
 
 
