62405e8f66a400830d6951f4c96b6d0f56842448
Javier <javierperez@javiers-mbp.home>
2021-01-02 10:28:50 -0500

    post expense route

--- controllers/api/expensesRoutes.js
+++ controllers/api/expensesRoutes.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
-// const {Expense} = require('../../models');
+const {
+    Expense
+} = require('../../models');
 
-// router.create('/',()=>{
+router.post('/', async (req, res) => {
+    try {
+        const newExpense = await Expense.create({
+           name:req.body.name,
+           description:req.body.description,
+           amount: req.body.amount,
+           user_id: req.session.user_id,
+           category_id:req.body.category_id
+        });
+        res.status(200).json(newExpense);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 
-// })
-
+});
 
 
 
