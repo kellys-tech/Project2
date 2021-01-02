cc4f61276449969d43049a616c68d12a8fadb6fb
Javier <javierperez@javiers-mbp.home>
2021-01-02 11:22:58 -0500

    expense routes

--- controllers/api/expensesRoutes.js
+++ controllers/api/expensesRoutes.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
-const {
-    Expense
-} = require('../../models');
+const { Expense, User, Category } = require('../../models');
 
+router.get('/', async (req, res) => {
+    const blogInfo = await Expense.findAll({
+        include: [{ model: User }, { model: Category }],
+    });
+    return res.json(blogInfo);
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const expenseData = await Expense.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        res.status(200).json(expenseData);
+    } catch (err) {
+        throw err;
+    }
+});
 router.post('/', async (req, res) => {
     try {
         const newExpense = await Expense.create({
@@ -16,8 +34,8 @@ router.post('/', async (req, res) => {
     } catch (err) {
         res.status(400).json(err);
     }
-
 });
+
 
 
 
