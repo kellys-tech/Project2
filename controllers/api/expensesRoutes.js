4b1adaa1f344fedbb0d531776cf78c9b7f268486
Javier <javierperez@javiers-mbp.home>
2021-01-02 11:05:58 -0500

    post expense

--- controllers/api/expensesRoutes.js
+++ controllers/api/expensesRoutes.js
@@ -21,6 +21,21 @@ router.delete('/:id', async (req, res) => {
         throw err;
     }
 });
+router.post('/', async (req, res) => {
+    try {
+        const newExpense = await Expense.create({
+           name:req.body.name,
+           description:req.body.description,
+           amount: req.body.amount,
+           user_id: req.session.user_id,
+           category_id:req.body.category_id
+        });
+        res.status(200).json(newExpense);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+
+});
 
 
 
