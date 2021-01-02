ad4a10c4c5aa0da6c779ddb8840256b60f2d58d0
Javier <javierperez@javiers-mbp.home>
2021-01-02 09:46:37 -0500

    routes modif

---
+++ controllers/api/expensesRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const {Expense} = require('../../models');
+
+// router.get('/',())
+
+
+
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const categoryRoutes = require('./categoryRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/category', categoryRoutes);
 
 module.exports = router;
