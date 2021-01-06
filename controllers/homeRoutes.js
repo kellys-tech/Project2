b5bf8ad3893f696d3d93b5346d9810cf8b23b36d
nbmc1991 <nbmc1991@gmail.com>
2021-01-06 18:31:58 -0500

    updating

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,103 +1,131 @@
 const router = require('express').Router();
-const { Category,Expense} = require('../models');
+const { Category, Expense } = require('../models');
 const withAuth = require('../utils/withAuth');
 
-
-
-router.get('/expenses',withAuth,async(req,res)=>{
-try{
-    const allExpenses = await Expense.findAll({
-        where:{
-            user_id:req.session.user_id
-        },include:[{
-            model:Category,
-            attributes:['name']
-        }]
-    });
-
-    let expensesArray = allExpenses.map((expense)=> expense.get({plain:true}));
-console.log(expensesArray);
-    res.json(expensesArray);
-    // res.render('home',{
-    //     logged_in:req.session.logged_in,
-    //     expensesArray
-    // });
-} catch(err) {
-    res.status(400).json(err);
-    }
-});
-router.get('/expenses/category/:id',withAuth,async(req,res)=>{
-    try{
-        const category1 =await Expense.findAll({
-            where:{
-                category_id:req.params.id,
-                user_id:req.session.user_id
-            },include:[{
-                model:Category,
-                attributes:['name']
-            }]
+router.get('/', async (req, res) => {
+    try {
+        const expenseData = await Category.findAll({
+            include: [{
+                model: Expense,
+                attributes: ['name', 'description', 'amount', 'date_created', 'category_id'],
+            },
+            ],
         });
-
-
-        const listOfExpenses = category1.map(expense => expense.get({plain:true}));
-res.json(listOfExpenses);
-        // res.render('category1',{
-        //     logged_in:req.session.logged_in,
-        //     listOfExpenses
-        // });
-    } catch(err){
-        res.status(400).json(err);
-    }
-});
-router.get('/expenses/month/:id',withAuth,async(req,res)=>{
-    try{
-        const category1 =await Expense.findAll({
-            where:{
-                date_created:req.params.id,
-                user_id:req.session.user_id
-            },include:[{
-                model:Category,
-                attributes:['name']
-            }]
+        const categories = expenseData.map((category) => category.get({ plain: true }));
+        //pass serializes data and sess into template
+        res.render('dashboard', {
+            categories,
+            // eslint-disable-next-line camelcase
+            logged_in: req.session.logged_in
         });
-
-
-        const listOfExpenses = category1.map(expense => expense.get({plain:true}));
-res.json(listOfExpenses);
-        // res.render('category1',{
-        //     logged_in:req.session.logged_in,
-        //     listOfExpenses
-        // });
-    } catch(err){
-        res.status(400).json(err);
+    } catch (err) {
+        res.status(500).json(err.message);
     }
 });
 
-router.get('/total',withAuth,async(req,res)=>{
-    try{
+
+
+router.get('/expenses', async (req, res) => {
+    try {
         const allExpenses = await Expense.findAll({
-            where:{
-                user_id:req.session.user_id
-            },include:[{
-                model:Category,
-                attributes:['name']
+            where: {
+                user_id: req.session.user_id
+            }, include: [{
+                model: Category,
+                attributes: ['name']
             }]
         });
 
-        let expensesArray = allExpenses.map((expense)=> expense.get({plain:true}));
+        let expensesArray = allExpenses.map((expense) => expense.get({ plain: true }));
+        console.log(expensesArray);
+        res.json(expensesArray);
+        // res.render('home',{
+        //     logged_in:req.session.logged_in,
+        //     expensesArray
+        // });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-        const totalExpenses = expensesArray.map((expense)=> expense.amount);
-        const total = totalExpenses.reduce((a,b) => a +b );
-res.json(total);
+router.get('/expenses/category/:id', withAuth, async (req, res) => {
+    try {
+        const category1 = await Expense.findAll({
+            where: {
+                category_id: req.params.id,
+                user_id: req.session.user_id
+            }, include: [{
+                model: Category,
+                attributes: ['name']
+            }]
+        });
+
+
+        const listOfExpenses = category1.map(expense => expense.get({ plain: true }));
+        res.json(listOfExpenses);
+        // res.render('category1',{
+        //     logged_in:req.session.logged_in,
+        //     listOfExpenses
+        // });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+router.get('/expenses/month/:id', withAuth, async (req, res) => {
+    try {
+        const category1 = await Expense.findAll({
+            where: {
+                date_created: req.params.id,
+                user_id: req.session.user_id
+            }, include: [{
+                model: Category,
+                attributes: ['name']
+            }]
+        });
+
+
+        const listOfExpenses = category1.map(expense => expense.get({ plain: true }));
+        res.json(listOfExpenses);
+        // res.render('category1',{
+        //     logged_in:req.session.logged_in,
+        //     listOfExpenses
+        // });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.get('/total', withAuth, async (req, res) => {
+    try {
+        const allExpenses = await Expense.findAll({
+            where: {
+                user_id: req.session.user_id
+            }, include: [{
+                model: Category,
+                attributes: ['name']
+            }]
+        });
+
+        let expensesArray = allExpenses.map((expense) => expense.get({ plain: true }));
+
+        const totalExpenses = expensesArray.map((expense) => expense.amount);
+        const total = totalExpenses.reduce((a, b) => a + b);
+        res.json(total);
         // res.render('total',{
         //     logged_in:req.session.logged_in,
         //     totalExpenses
         // });
-    } catch(err) {
+    } catch (err) {
         res.status(400).json(err);
-        }
-    });
+    }
+});
 
-
-
+//login route
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('dashboard');
+        return;
+    }
+    res.render('login');
+});
 module.exports = router;
