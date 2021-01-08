8025562eed1f5dbce0bbc2f73b2487bf56231143
nbmc1991 <nbmc1991@gmail.com>
2021-01-07 21:07:05 -0500

    handlebars changes

---
+++ public/js/dashboard.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#blog-name').value.trim();
+    const description = document.querySelector('#blog-funding').value.trim();
+    // const content = document.querySelector('#blog-content').value.trim();
+
+    if (name && description) {
+
+        const response = await fetch(`/api/categories`, {
+            method: 'POST',
+            body: JSON.stringify({ name, description }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create blog');
+        }
+    }
+};
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/category/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete blog');
+        }
+    }
+};
+
+document
+    .querySelector('.new-blog-form')
+    .addEventListener('submit', newFormHandler);
+
+document
+    .querySelector('.blog-list')
+    .addEventListener('click', delButtonHandler);
